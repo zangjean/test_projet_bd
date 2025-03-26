2929d7c55ddf8c9a00bc3d02327584ca92ec3c42
sergefavy <81034972+sergefavy@users.noreply.github.com>
2025-03-26 14:45:34 +0100

    add controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UserController extends AbstractController{
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        return $this->render('user.html.twig');
+    }
+}
